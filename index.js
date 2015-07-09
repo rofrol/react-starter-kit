cb70699941433f91d6d4635db69ee6b351a7907a
Roman Frołow <rofrol@gmail.com>
2015-07-10 01:56:46 +0200

    two-way data binding works

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 import hex from 'hex-rgb'
 
 console.log(hex('#abde13').map(function(x) { return x/255} ))
+
+import React from 'react'
+
+// https://facebook.github.io/react/docs/two-way-binding-helpers.html
+var NoLink = React.createClass({
+  getInitialState: function() {
+    return {message: 'Hello!'};
+  },
+  handleChange: function(event) {
+    this.setState({message: event.target.value});
+  },
+  render: function() {
+    var message = this.state.message;
+    return (
+    	<div>
+	    	<h3>{this.state.message}</h3>
+	    	<input type="text" value={message} onChange={this.handleChange} />
+    	</div>
+    )
+  }
+});
+
+ React.render(
+	<NoLink txt="some text" />,
+	document.getElementById('app')
+);
