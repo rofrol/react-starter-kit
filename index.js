5d7a2129922c929f0222327306f05f72489bc071
Roman Frołow <rofrol@gmail.com>
2015-11-18 17:18:42 +0100

    li

--- index.js
+++ index.js
@@ -52,7 +52,17 @@ ReactDOM.render(
 );
 
 var rootElement = React.createElement('div', {},
-	React.createElement('h1', {}, 'Hello')
+	React.createElement('h1', {}, 'Contacts'),
+	React.createElement('ul', {},
+		React.createElement('li', {},
+			React.createElement('h2', {}, 'John Doe'),
+			React.createElement('a', {href: 'mailto:john.doe@example.com'}, 'john.doe@example.com')
+		),
+		React.createElement('li', {},
+			React.createElement('h2', {}, 'Joe Pesci'),
+			React.createElement('a', {href: 'mailto:joe.pesci@example.com'}, 'joe.pesci@example.com')
+		)
+	)
 );
 
 ReactDOM.render(rootElement, document.querySelector('#raw-reactjs'));
