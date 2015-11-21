67191d829d51935ada95ab4b989b3fb36633176e
Roman Frołow <rofrol@gmail.com>
2015-11-21 20:55:23 +0100

    use strict

--- check_env.js
+++ check_env.js
@@ -1,2 +1,4 @@
+"use strict";
+
 console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV);
 console.log('database: ' + process.env[process.env.NODE_ENV + '_database']);
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // https://www.reddit.com/r/javascript/comments/332v73/is_anyone_using_es6_in_a_large_project_hows_it/cqh2u7i
 import hex from 'hex-rgb';
 
